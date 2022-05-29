f23b82888209095ac976e32ea329ad5277ad16a9
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2022-05-29 17:07:34 +0100

    Updated client/src/pages/login/Login.jsx

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,29 +1,60 @@
-import './login.css'
+import { useRef } from "react";
+import "./login.css";
+import { loginCall } from "../../apiCalls";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 
 function Login() {
+  const email = useRef();
+  const password = useRef();
+  const { user, isFetching, error, dispatch } = useContext(AuthContext);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    loginCall({ email: email.current.value, password: password.current.value }, dispatch);
+  };
+
+  console.log(user)
+
   return (
     <div className="login">
-        <div className="loginWrapper">
-            <div className="loginLeft">
-                <h3 className="loginLogo">Pokemesocial</h3>
-                <span className="loginDesc">
-                    Connect with friends and the world around you on Pokemsocial
-                </span>
-            </div>
-            <div className="loginRight">
-                <div className="loginBox">
-                    <input placeholder="Email" className="loginInput" />
-                    <input placeholder="Password" className="loginInput" />
-                    <button className="loginButton">Log In</button>
-                    <span className="loginForgot">Forgot Password?</span>
-                    <button className="loginRegisterButton">
-                        Create a New Account
-                    </button>
-                </div>
-            </div>
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Pokemesocial</h3>
+          <span className="loginDesc">
+            Connect with friends and the world around you on Pokemsocial
+          </span>
         </div>
+        <div className="loginRight">
+          <form className="loginBox" onSubmit={handleClick}>
+            <input
+              placeholder="Email"
+              type="email"
+              className="loginInput"
+              required
+              ref={email}
+            />
+            <input
+              placeholder="Password"
+              type="password"
+              className="loginInput"
+              minLength="6"
+              required
+              ref={password}
+            />
+            <button className="loginButton" disabled={isFetching}>{isFetching? <CircularProgress color="white" size="1em" /> :"Log In"}</button>
+            <span className="loginForgot">Forgot Password?</span>
+            <button className="loginRegisterButton">
+            {isFetching? <CircularProgress color="white" size="1em" /> :"Create a New Account"}
+              
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
