f9995b74ea84d65765de16dcb26cc64e9daf677a
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2022-06-25 11:34:20 +0100

    Fixed issues
    Fuxed issue with the image uploader and also fixed javascript-time-ago issue

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -2,10 +2,14 @@ import "./post.css"
 import {MoreVert} from '@material-ui/icons';
 import { useEffect, useState } from "react";
 import axios from "axios"
-import {format} from "timeago.js"
+/* import { format} from 'timeago.js'; */
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
+
+
 
 
 function Post({post}) {
@@ -14,6 +18,9 @@ function Post({post}) {
     const [user, setUser] = useState({})
     const PF = process.env.REACT_APP_PUBLIC_FOLDER
     const {user:currentUser} = useContext(AuthContext)
+
+    TimeAgo.addDefaultLocale(en)
+    const timeAgo = new TimeAgo('en-US')
 
     useEffect(()=>{
         setIsLiked(post.likes.includes(currentUser._id))
@@ -39,38 +46,56 @@ function Post({post}) {
         setIsLiked(!isLiked)
     }
   return (
-      <div className="post">
-          <div className="postWrapper">
-            <div className="postTop">
-                <div className="postTopLeft">
-                    <Link to={`/profile/${user.username}`}>
-                        <img className="postProfileImg" src={user.profilePicture ? PF+user.profilePicture : PF+"person/noAvatar.png"} alt="" />
-                    </Link>
-                    <span className="postUsername">{user.username}</span>
-                    <span className="postDate">{format(post.createdAt)}</span>
-                </div>
-                <div className="postTopRight">
-                    <MoreVert />
-                </div>         
-            </div>
-
-            <div className="postCenter">
-                <span className="postText">{post?.desc}</span>
-                <img src={PF+post.img} alt="" className="postImg" />
-            </div>
-            <div className="postBottom">
-                <div className="postBottomLeft">
-                    <img className="likeIcon" src={`${PF}like.png`} onClick={likeHandler} alt="" />
-                    <img className="likeIcon" src={`${PF}heart.png`} onClick={likeHandler} alt="" />
-                    <div className="postLikeCounter">{like} people like it</div>
-                </div>
-                <div className="postBottomRight">
-                    <span className="postCommentText">{post.comment} comments</span>
-                </div>
-            </div>
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <Link to={`/profile/${user.username}`}>
+              <img
+                className="postProfileImg"
+                src={
+                  user.profilePicture
+                    ? PF + user.profilePicture
+                    : PF + "person/noAvatar.png"
+                }
+                alt=""
+              />
+            </Link>
+            <span className="postUsername">{user.username}</span>
+            <span className="postDate">{timeAgo.format(new Date(post.createdAt))}</span>
           </div>
-      </div>  
-  )
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+
+        <div className="postCenter">
+          <span className="postText">{post?.desc}</span>
+          <img src={PF + post.img} alt="" className="postImg" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img
+              className="likeIcon"
+              src={`${PF}like.png`}
+              onClick={likeHandler}
+              alt=""
+            />
+            <img
+              className="likeIcon"
+              src={`${PF}heart.png`}
+              onClick={likeHandler}
+              alt=""
+            />
+            <div className="postLikeCounter">{like} people like it</div>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">{post.comment} comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Post
