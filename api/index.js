7109a25d7f79ffa45e932e84199dcd9bed8a51ed
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2022-07-23 05:44:50 -0400

    removed redundant line of code

--- api/index.js
+++ api/index.js
@@ -1,19 +1,17 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const helmet = require('helmet')
-const morgan = require('morgan')
-const { response } = require('express')
-const userRoute = require('./routes/users')
-const authRoute = require('./routes/auth')
-const postRoute = require('./routes/posts')
-const port = 8800
-const multer  = require('multer')
-const path = require('path')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const port = 8800;
+const multer = require("multer");
+const path = require("path");
 
-
-dotenv.config()
+dotenv.config();
 
 /* mongoose.connect(process.env.MONGO_URL,
   {useNewUrlParser: true, useUnifiedTopology: true,},
@@ -21,40 +19,40 @@ dotenv.config()
     console.log("Connected to MongoDB")
 }) */
 
-mongoose.connect(process.env.MONGO_URL,()=>{console.log("Connected to MongoDB")})
+mongoose.connect(process.env.MONGO_URL, () => {
+  console.log("Connected to MongoDB");
+});
 
-app.use("/images", express.static(path.join(__dirname, "public/images")))
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //middleware
-app.use(express.json())
-app.use(helmet())
-app.use(morgan("common"))
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) =>{
-    
-    cb(null, "public/images")
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
   },
   filename: (req, file, cb) => {
-    cb(null, req.body.name)
-  }
-})
+    cb(null, req.body.name);
+  },
+});
 
-const upload = multer({storage})
+const upload = multer({ storage });
 
-app.post("/api/upload",upload.single("file"),(req,res)=>{
+app.post("/api/upload", upload.single("file"), (req, res) => {
   try {
-    return res.status(200).json("File uploaded successfully.")
+    return res.status(200).json("File uploaded successfully.");
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-})
+});
 
-app.use('/api/users', userRoute)
-app.use('/api/auth', authRoute)
-app.use('/api/posts', postRoute)
-
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(port, () => {
-  console.log(`Backend server is running on ${port}`)
-})
+  console.log(`Backend server is running on ${port}`);
+});
